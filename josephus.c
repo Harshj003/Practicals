0bd61608dceef284a210451bef234ba22864d7fb
Harshj003 <91816454+Harshj003@users.noreply.github.com>
2023-01-18 21:37:10 +0530

    Create josephus.c

---
+++ josephus.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+struct node
+{
+ int player_id;
+ struct node *next;
+};
+struct node *start,*ptr,*new_node;
+int main()
+{
+ int n,k,i,count;
+ printf("\n Enter the number of players:");
+ scanf("%d",&n);
+ printf("\n Enter the value of k:");
+ scanf("%d", &k);
+ start=malloc(sizeof(struct node));
+ start->player_id=1;
+ ptr=start;
+ for(i=2;i<=n;i++)
+ {
+ new_node=malloc(sizeof(struct node));
+ ptr->next=new_node;
+ new_node->player_id=i;
+ new_node->next=start;
+ ptr=new_node;
+ }
+ for(count=n;count>1;count--)
+ {
+ for(i=0;i<k-1;++i)
+ ptr=ptr->next;
+ printf("%d Deleted \n",ptr->next->player_id);
+ ptr->next=ptr->next->next;
+ }
+ printf("\n The winner is playeer: %d",ptr->player_id);
+ return 0;
+}
